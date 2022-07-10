d84629a41931d7fc1c81af7512ebc959c993c0be
Konstantin Stroganov <constantine.stroganov@gmail.com>
2022-07-10 14:32:11 +0300

    Show test message on get request

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.post('/register', function(request, response){
    console.log(request.body);      // your JSON
    response.send(request.body);    // echo the result back
 });
+app.get('/', (req, res) => {
+  // Use req.log (a `pino` instance) to log JSON:
+  res.send('This message is displayed for testing purposes.');
+});
 
 const PORT = process.env.PORT || 3000;
 
