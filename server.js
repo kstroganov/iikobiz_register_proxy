95dc2d40f2bf9dc99b35e7b4a15ae804572dad23
Konstantin Stroganov <constantine.stroganov@gmail.com>
2022-07-10 15:55:59 +0300

    Send message to Whatsapp - attemption 1

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 var express = require('express')
 var bodyParser = require('body-parser');
+var http = require('https');
+
+var options = {
+  host: 'graph.facebook.com',
+  path: '/v13.0/109844545118491/messages',
+  headers: {
+	  'Authorization': 'Bearer EAAMyDtny59sBAI6PgS0v3nifjlEnhoJGBBBXSvp7n7ZCxhUN9koaxSZA6gYhkyzwnwIKtL1iqnbuErl5RpxszzkXrXqZCD3Qy47GiWHDZBnhwLmIa4Esim67EtIZALXlzBQslHH1jHXezmTzdyJoUZBZA9YrWZAc98z3gAIIpZAKGc1meeAGaZBMjvirbsEtNQNmyARG7P6aeZBA42qYZAPP64k0',
+	  'Content-Type': 'application/json' },
+  method: 'POST'
+};
+
+callback = function(response) {
+  var str = '';
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  response.on('end', function () {
+    console.log(str);
+  });
+}
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/register', function(request, response){
+app.post('/register', function(request, response) {
+   var req = http.request(options, callback);
+   req.write('{ "messaging_product": "whatsapp", "to": "789160378158", "type": "template", "template": { "name": "hello_world", "language": { "code": "en_US" } } }');
+   req.end();
    console.log(request.body);      // your JSON
    response.send(request.body);    // echo the result back
 });
